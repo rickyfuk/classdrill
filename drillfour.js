dcc1cb702ba90cb330fbab2710822ab596cbc0da
digilau <rickyfukfuk@gmail.com>
2020-07-11 14:43:56 -0700

    initial commit

---
+++ drillfour.js
@@ -0,0 +1,29 @@
+var numArray = [];
+var total = 0;
+
+function addNum() {
+	var needNum = confirm('Do you need another number?');
+	if (needNum) {
+		var inputByUser = parseInt(prompt('please enter any number'));
+		numArray.push(inputByUser);
+		addNum();
+	}
+}
+addNum();
+console.log(numArray);
+console.log(numArray[1]);
+console.log(numArray.length);
+
+newFunction();
+// console.log(total + numArray[1]);
+var average = total / numArray.lenght;
+console.log(average);
+
+function newFunction() {
+	console.log(numArray);
+	for (var i = 0; i < numArray.lenght; i++) {
+		// total = total + numArray[i];
+		console.log('for loop is running');
+		// console.log(total);
+	}
+}
